105c8d63979640f58f315cefeac9dec634fb551a
Rico Picone <rpicone@stmartin.edu>
2024-02-05 01:49:25 -0800

    Publishing itself actually working now

--- engcom/publish.py
+++ engcom/publish.py
@@ -9,26 +9,30 @@ import pypandoc
 
 class Publication:
     def __init__(self, title, author, source_filename=None, source_kind="script"):
-        self.title = title
-        self.author = author
-        if not source_filename: # Then use the calling file
-            source_filename = pathlib.Path(
-                inspect.getframeinfo(sys._getframe(1)).filename
-            )
-        self.subtitle = "Source Filename: "\
-            + f"{source_filename.parent.stem}/{source_filename.name}"
-        self.source_kind = source_kind
-        self.source_filename = source_filename
-        self.basename = self.basenamer(source_filename)
-        self.jupytext = jupytext.read(source_filename)
-        if source_kind == "script":
-            self.write(to="ipynb-tmp")
-            self.pypandoc = pypandoc.convert_file(f".tmp_{self.basename}.ipynb", 'rst')
-            self.cleanup()
-        elif source_kind == "ipynb":
-            self.pypandoc = pypandoc.convert_file(source_filename, 'rst')
+        if self.is_notebook():
+            self.nowrite = True # Don't want to write if executed from within an ipynb (infinite loop)
         else:
-            raise ValueError(f"Unknown source_kind={source_kind}")
+            self.nowrite = False
+            self.title = title
+            self.author = author
+            if not source_filename: # Then use the calling file
+                source_filename = pathlib.Path(
+                    inspect.getframeinfo(sys._getframe(1)).filename
+                )
+            self.subtitle = "Source Filename: "\
+                + f"{source_filename.parent.stem}/{source_filename.name}"
+            self.source_kind = source_kind
+            self.source_filename = source_filename
+            self.basename = self.basenamer(source_filename)
+            self.jupytext = jupytext.read(source_filename)
+            if source_kind == "script":
+                self.write(to="ipynb-tmp")
+                self.pypandoc = pypandoc.convert_file(f".tmp_{self.basename}.ipynb", 'rst')
+                self.cleanup()
+            elif source_kind == "ipynb":
+                self.pypandoc = pypandoc.convert_file(source_filename, 'rst')
+            else:
+                raise ValueError(f"Unknown source_kind={source_kind}")
             
     def basenamer(self, filename):
         return pathlib.Path(filename).stem
@@ -49,42 +53,56 @@ class Publication:
         return pathlib.Path(__file__).parent / "pandoc_reference.docx"
         
     def write(self, to):
-        if to == "ipynb":
-            jupytext.write(self.jupytext, f"{self.basename}_pub.ipynb")
-        elif to == "ipynb-tmp":
-            jupytext.write(self.jupytext, f".tmp_{self.basename}.ipynb")
-        elif to == "md" or to == "pdf" or to == "docx":
-            self.run()
-            tmp_nb_executed = f".tmp_{self.basename}_executed.ipynb"
-            if to == "md":
-                self.write(to="ipynb-tmp")
-                output = pypandoc.convert_file(tmp_nb_executed, 'md', outputfile=f"{self.basename}_pub.md")
-                assert output == ""
-            elif to == "pdf":
-                self.write(to="ipynb-tmp")
-                output = pypandoc.convert_file(tmp_nb_executed, 'pdf', outputfile=f"{self.basename}_pub.pdf")
-                assert output == ""
-            elif to == "docx":
-                self.write(to="ipynb-tmp")
-                filters = [str(self.filter_absolute_path())]
-                extra_args = [
-                    "--reference-doc", str(self.reference_doc_absolute_path()),
-                    f"--metadata=title:{self.title}",
-                    f"--metadata=subtitle:{self.subtitle}",
-                    f"--metadata=author:{self.author}",
-                ]
-                output = pypandoc.convert_file(
-                    tmp_nb_executed, 'docx', 
-                    outputfile=f"{self.basename}_pub.docx",
-                    extra_args=extra_args,
-                    filters=filters
-                )
-                assert output == ""
+        if self.nowrite:
+            return None
         else:
-            raise ValueError(f"Unkown target (to) format: {to}")
+            if to == "ipynb":
+                jupytext.write(self.jupytext, f"{self.basename}_pub.ipynb")
+            elif to == "ipynb-tmp":
+                jupytext.write(self.jupytext, f".tmp_{self.basename}.ipynb")
+            elif to == "md" or to == "pdf" or to == "docx":
+                self.run()
+                tmp_nb_executed = f".tmp_{self.basename}_executed.ipynb"
+                if to == "md":
+                    self.write(to="ipynb-tmp")
+                    output = pypandoc.convert_file(tmp_nb_executed, 'md', outputfile=f"{self.basename}_pub.md")
+                    assert output == ""
+                elif to == "pdf":
+                    self.write(to="ipynb-tmp")
+                    output = pypandoc.convert_file(tmp_nb_executed, 'pdf', outputfile=f"{self.basename}_pub.pdf")
+                    assert output == ""
+                elif to == "docx":
+                    self.write(to="ipynb-tmp")
+                    filters = [str(self.filter_absolute_path())]
+                    extra_args = [
+                        "--reference-doc", str(self.reference_doc_absolute_path()),
+                        f"--metadata=title:{self.title}",
+                        f"--metadata=subtitle:{self.subtitle}",
+                        f"--metadata=author:{self.author}",
+                    ]
+                    output = pypandoc.convert_file(
+                        tmp_nb_executed, 'docx', 
+                        outputfile=f"{self.basename}_pub.docx",
+                        extra_args=extra_args,
+                        filters=filters
+                    )
+                    assert output == ""
+            else:
+                raise ValueError(f"Unkown target (to) format: {to}")
         
     def cleanup(self):
         for filename in pathlib.Path(".").glob(".tmp_{self.basename}*"):
             filename.unlink()
-            
+    
+    def is_notebook(self) -> bool:
+        try:
+            shell = get_ipython().__class__.__name__
+            if shell == 'ZMQInteractiveShell':
+                return True   # Jupyter notebook or qtconsole
+            elif shell == 'TerminalInteractiveShell':
+                return False  # Terminal running IPython
+            else:
+                return False  # Other type (?)
+        except NameError:
+            return False      # Probably standard Python interpreter
             
