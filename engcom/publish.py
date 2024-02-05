343f9e80348156b610dc21b970e85f8b13113792
Rico Picone <rpicone@stmartin.edu>
2024-02-05 00:54:39 -0800

    Functional Publication class

---
+++ engcom/publish.py
@@ -0,0 +1,81 @@
+import jupytext
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
+import pathlib
+import os
+import pypandoc
+
+class Publication:
+    def __init__(self, source_filename, title, author, source_kind="script"):
+        self.title = title
+        self.author = author
+        self.source_kind = source_kind
+        self.source_filename = pathlib.Path(source_filename)
+        self.basename = self.basenamer(source_filename)
+        self.jupytext = jupytext.read(source_filename)
+        if source_kind == "script":
+            self.write(to="ipynb-tmp")
+            self.pypandoc = pypandoc.convert_file(f".tmp_{self.basename}.ipynb", 'rst')
+            self.cleanup()
+        elif source_kind == "ipynb":
+            self.pypandoc = pypandoc.convert_file(source_filename, 'rst')
+        else:
+            raise ValueError(f"Unknown source_kind={source_kind}")
+            
+    def basenamer(self, filename):
+        return pathlib.Path(filename).stem
+    
+    def run(self):
+        self.write(to="ipynb-tmp")
+        with open(f".tmp_{self.basename}.ipynb") as f:
+            nb = nbformat.read(f, as_version=4)
+        ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
+        ep.preprocess(nb, {'metadata': {'path': '.'}})
+        with open(f".tmp_{self.basename}_executed.ipynb", 'w', encoding='utf-8') as f:
+            nbformat.write(nb, f)
+    
+    def filter_absolute_path(self):
+        return pathlib.Path(__file__).parent / "filter.lua"
+    
+    def reference_doc_absolute_path(self):
+        return pathlib.Path(__file__).parent / "pandoc_reference.docx"
+        
+    def write(self, to):
+        if to == "ipynb":
+            jupytext.write(self.jupytext, f"{self.basename}_pub.ipynb")
+        elif to == "ipynb-tmp":
+            jupytext.write(self.jupytext, f".tmp_{self.basename}.ipynb")
+        elif to == "md" or to == "pdf" or to == "docx":
+            self.run()
+            tmp_nb_executed = f".tmp_{self.basename}_executed.ipynb"
+            if to == "md":
+                self.write(to="ipynb-tmp")
+                output = pypandoc.convert_file(tmp_nb_executed, 'md', outputfile=f"{self.basename}_pub.md")
+                assert output == ""
+            elif to == "pdf":
+                self.write(to="ipynb-tmp")
+                output = pypandoc.convert_file(tmp_nb_executed, 'pdf', outputfile=f"{self.basename}_pub.pdf")
+                assert output == ""
+            elif to == "docx":
+                self.write(to="ipynb-tmp")
+                filters = [str(self.filter_absolute_path())]
+                extra_args = [
+                    "--reference-doc", str(self.reference_doc_absolute_path()),
+                    f"--metadata=title:{self.title}",
+                    f"--metadata=author:{self.author}",
+                ]
+                output = pypandoc.convert_file(
+                    tmp_nb_executed, 'docx', 
+                    outputfile=f"{self.basename}_pub.docx",
+                    extra_args=extra_args,
+                    filters=filters
+                )
+                assert output == ""
+        else:
+            raise ValueError(f"Unkown target (to) format: {to}")
+        
+    def cleanup(self):
+        for filename in pathlib.Path(".").glob(".tmp_{self.basename}*"):
+            filename.unlink()
+            
+            
